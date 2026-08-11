d82622d816269ba1267a9befcf6f00bf30427f06
Pankaj Mishra (Ernst and Young LLP) <v-mishrapank@microsoft.com>
2026-08-11 01:05:02 -0700

    test

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
   use_oidc = true
