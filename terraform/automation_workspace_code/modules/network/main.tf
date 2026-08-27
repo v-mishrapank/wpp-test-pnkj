44b5e1b6d226479483b49aec38ebce3965462dd7
Pankaj Mishra (Ernst and Young LLP) <v-mishrapank@microsoft.com>
2026-08-27 06:54:48 -0700

    modifing VMs for access issue

--- terraform/automation_workspace_code/modules/network/main.tf
+++ terraform/automation_workspace_code/modules/network/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "this" {
   name                = "${var.resource_group_name}-vnet-01"
   address_space       = var.vnet_address_space
   location            = var.location
@@ -6,11 +6,11 @@ resource "azurerm_virtual_network" "main" {
   tags                = var.tags
 }
 
-resource "azurerm_subnet" "subnets" {
-  for_each = var.subnet_prefixes
+resource "azurerm_subnet" "this" {
+  for_each = var.subnets
 
-  name                 = "${var.resource_group_name}-${each.key}-subnet"
+  name                 = each.key
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = [each.value.address_prefixes]
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = each.value.address_prefixes
 }
