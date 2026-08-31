97a61e2a065405e55d536effb4aa2bcb521af1d9
Pankaj Mishra (Ernst and Young LLP) <v-mishrapank@microsoft.com>
2026-08-31 04:39:23 -0700

    modifing VMs for access issue

--- terraform/automation_workspace_code/main.tf
+++ terraform/automation_workspace_code/main.tf
@@ -10,8 +10,8 @@ module "network" {
   source = "./modules/network"
 
   vnet_name           = var.vnet_name
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   vnet_address_space  = var.vnet_address_space
   dns_servers         = var.dns_servers
 
