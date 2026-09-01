6be0582fc29a82dd1019f452f989cd816a85638d
Pankaj Mishra (Ernst and Young LLP) <v-mishrapank@microsoft.com>
2026-09-01 02:44:23 -0700

    modifing VMs for access issue

--- terraform/automation_workspace_code/locals.tf
+++ terraform/automation_workspace_code/locals.tf
@@ -142,19 +142,3 @@ locals {
     ManagedBy   = "Terraform"
   }
 
-locals {
-  security_rules = merge(
-    [
-      for nsg_key, nsg in var.nsgs : {
-        for rule_key, rule in nsg.security_rules :
-        "${nsg_key}-${rule_key}" => merge(
-          rule,
-          {
-            nsg_key  = nsg_key
-            rule_key = rule_key
-          }
-        )
-      }
-    ]...
-  )
-}
