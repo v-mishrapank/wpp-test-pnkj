10d6a0e81503e14c44bfd4a99ecfdd020288b3d0
Pankaj Mishra (Ernst and Young LLP) <v-mishrapank@microsoft.com>
2026-09-01 11:22:41 -0700

    modifing VMs for access issue

--- terraform/automation_workspace_code/modules/storage/main.tf
+++ terraform/automation_workspace_code/modules/storage/main.tf
@@ -33,5 +33,5 @@ resource "azurerm_storage_container" "containers" {
 
   name                  = each.value
   storage_account_name  = azurerm_storage_account.function.name
-  container_access_type = "public"
+  container_access_type = "private"
 }
