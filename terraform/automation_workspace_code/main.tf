176d19020985673f2816131f5e4c37a6fdfe33ca
Pankaj Mishra (Ernst and Young LLP) <v-mishrapank@microsoft.com>
2026-09-04 04:41:13 -0700

    modifing VMs for access issue

--- terraform/automation_workspace_code/main.tf
+++ terraform/automation_workspace_code/main.tf
@@ -191,7 +191,7 @@ module "acr_hub" {
 
 
 resource "azurerm_container_app_environment" "hub" {
-  name                       = "cae-wpp-hub-${var.env}"
+  name                       = "cae-wpp-hub-${var.env}-01"
   location                   = azurerm_resource_group.this.location
   resource_group_name        = azurerm_resource_group.this.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.this["container"].id
